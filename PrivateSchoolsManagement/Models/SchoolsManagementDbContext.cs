eb8841d1b2cd51fb63d11b8967e09bc8fd56ee25
Marcin <Marcin@DESKTOP-L62L7NR>
2023-03-03 17:00:28 +0100

    Add db context class

---
+++ PrivateSchoolsManagement/Models/SchoolsManagementDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace PrivateSchoolsManagement.Models
+{
+    public class SchoolsManagementDbContext : DbContext
+    {
+        public SchoolsManagementDbContext(DbContextOptions<SchoolsManagementDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<School> Schools { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}

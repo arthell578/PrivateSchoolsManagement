43fc93f8b1d2d978926f1577a05dd79bcc5ef553
Marcin <Marcin@DESKTOP-L62L7NR>
2023-03-04 23:08:53 +0100

    Update service to use mapping

--- PrivateSchoolsManagement/Services/SchoolService.cs
+++ PrivateSchoolsManagement/Services/SchoolService.cs
@@ -1,18 +1,24 @@
-﻿using PrivateSchoolsManagement.Models;
+﻿using AutoMapper;
+using PrivateSchoolsManagement.DTOs;
+using PrivateSchoolsManagement.Interfaces;
+using PrivateSchoolsManagement.Models;
 
 namespace PrivateSchoolsManagement.Services
 {
     public class SchoolService : ISchoolService
     {
         private readonly SchoolsManagementDbContext _dbContext;
+        private readonly IMapper _mapper;
 
-        public SchoolService(SchoolsManagementDbContext dbContext)
+        public SchoolService(SchoolsManagementDbContext dbContext, IMapper mapper)
         {
             _dbContext = dbContext;
+            _mapper = mapper;
         }
 
-        public async Task CreateSchoolAsync(School school)
+        public async Task CreateSchoolAsync(SchoolDTO schoolDTO)
         {
+            var school = _mapper.Map<School>(schoolDTO);
             await _dbContext.Schools.AddAsync(school);
             await _dbContext.SaveChangesAsync();
         }

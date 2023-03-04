d74ecc2fada7e4a0cf765a284aed88eddaa1786f
Marcin <Marcin@DESKTOP-L62L7NR>
2023-03-04 22:11:39 +0100

    Add school controller CRUD

---
+++ PrivateSchoolsManagement/Controllers/SchoolController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrivateSchoolsManagement.Models;
+
+namespace PrivateSchoolsManagement.Controllers
+{
+    [Route("api/schools")]
+    [ApiController]
+    public class SchoolController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateSchool(School school)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllSchools()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSchoolById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateSchool(int id, School school)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteSchool(int id)
+        {
+            return Ok();
+        }
+    }
+
+}

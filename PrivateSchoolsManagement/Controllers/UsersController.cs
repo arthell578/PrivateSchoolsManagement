cb50dd873f122ab5a0686336b2a4d75d2c82e484
Marcin <Marcin@DESKTOP-L62L7NR>
2023-03-05 20:27:21 +0100

    Add controller methods

--- PrivateSchoolsManagement/Controllers/UsersController.cs
+++ PrivateSchoolsManagement/Controllers/UsersController.cs
@@ -1,6 +1,124 @@
-﻿namespace PrivateSchoolsManagement.Controllers
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PrivateSchoolsManagement.Interfaces;
+using PrivateSchoolsManagement.Models;
+using System.Data;
+using System.Security.Claims;
+
+namespace PrivateSchoolsManagement.Controllers
 {
+    [Route("api/account")]
+    [ApiController]
     public class UsersController
     {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> AuthenticateAsync(LoginModel model)
+        {
+            var user = await _userService.AuthenticateAsync(model.Username, model.Password);
+
+            if (user == null)
+            {
+                return BadRequest(new { message = "Username or password is incorrect" });
+            }
+
+            var claims = new[]
+            {
+            new System.Security.Claims.Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new System.Security.Claims.Claim(ClaimTypes.Name, user.Username),
+            new System.Security.Claims.Claim(ClaimTypes.Role, user.Role)
+        };
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new System.Security.Claims.ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme));
+
+            return Ok(user);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var users = await _userService.GetAllAsync();
+            return Ok(users);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            var user = await _userService.GetByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromBody] User user)
+        {
+            try
+            {
+                var createdUser = await _userService.CreateAsync(user);
+                return CreatedAtAction(nameof(GetByIdAsync), new { id = createdUser.Id }, createdUser);
+            }
+            catch (AppException ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [Authorize(Roles = "Owner,Admin")]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAsync(int id, [FromBody] User user)
+        {
+            // only allow admins to update other user records
+            if (id != user.Id && !User.IsInRole("Admin"))
+            {
+                return Forbid();
+            }
+
+            try
+            {
+                var updatedUser = await _userService.UpdateAsync(user);
+                return Ok(updatedUser);
+            }
+            catch (AppException ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [Authorize(Roles = "Owner,Admin")]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            // only allow admins to delete user records
+            if (!User.IsInRole("Admin"))
+            {
+                return Forbid();
+            }
+
+            await _userService.DeleteAsync(id);
+            return NoContent();
+        }
+
+        [AllowAnonymous]
+        [HttpPost("logout")]
+        public async Task<IActionResult> LogoutAsync()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Ok();
+        }
     }
 }

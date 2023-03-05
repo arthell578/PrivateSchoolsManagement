c27737c9cfefbfda6f1ebaa98f0b4a7c2f4e1ca2
Marcin <Marcin@DESKTOP-L62L7NR>
2023-03-05 20:20:47 +0100

    Add Helper class containing password creation and verification

---
+++ PrivateSchoolsManagement/Helpers/PasswordHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+
+namespace PrivateSchoolsManagement.Helpers
+{
+    public static class PasswordHelper
+    {
+        public static string HashPassword(string password)
+        {
+            // Generate a random salt value
+            byte[] salt = new byte[16];
+            new RNGCryptoServiceProvider().GetBytes(salt);
+
+            // Hash the password using Bcrypt with the salt
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password, salt);
+
+            // Combine the salt and hash values into a single string
+            string saltString = Convert.ToBase64String(salt);
+            return $"{saltString}:{hashedPassword}";
+        }
+
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            // Extract the salt value from the stored hash value
+            string[] parts = hashedPassword.Split(':');
+            byte[] salt = Convert.FromBase64String(parts[0]);
+
+            // Hash the password using Bcrypt with the same salt
+            string hash = BCrypt.Net.BCrypt.HashPassword(password, salt);
+
+            // Compare the resulting hash value with the stored hash value
+            return hash == hashedPassword;
+        }
+    }
+}

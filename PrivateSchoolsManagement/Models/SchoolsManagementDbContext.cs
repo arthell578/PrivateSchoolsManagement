b3b4e60abd4f92babd07b75639a50d2c5e156d57
Marcin <Marcin@DESKTOP-L62L7NR>
2023-03-05 18:27:17 +0100

    Add user entity and register in db context

--- PrivateSchoolsManagement/Models/SchoolsManagementDbContext.cs
+++ PrivateSchoolsManagement/Models/SchoolsManagementDbContext.cs
@@ -17,6 +17,7 @@ namespace PrivateSchoolsManagement.Models
         public DbSet<Student> Students { get; set; }
         public DbSet<Class> Classes { get; set; }
         public DbSet<Grade> Grades { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
---
+++ PrivateSchoolsManagement/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrivateSchoolsManagement.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public  DateTime Created { get; set; }
+    }
+}
